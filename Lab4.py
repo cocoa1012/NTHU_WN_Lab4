0e2e5b333618719a9b5026307e9a34885ce70201
cocoa104062221 <a2324309@gmail.com>
2019-10-04 23:10:21 +0800

    Cyclic Done

--- Lab4.py
+++ Lab4.py
@@ -3,19 +3,29 @@ import copy
 import numpy as np
 
 rawData = []
-f = open("output.txt","w")
+f = open("output.txt", "w")
+rawString = ""
 
-def randomRuin(data, codeType):    
+
+def randomRuin(data, codeType):
     if codeType == "Hamming":
         for i in range(0, len(data)):
-            if random.randint(0,1):
-                j = random.randint(0,11)
+            if random.randint(0, 1):
+                j = random.randint(0, 11)
                 data[i][j] ^= 1
         return data
     elif codeType == "LinearBlock":
-        pass
+        for i in range(0, len(data)):
+            if random.randint(0, 1):
+                j = random.randint(0, 6)
+                data[i][j] ^= 1
+        return data
     elif codeType == "Cyclic":
-        pass
+        for i in range(0, len(data)):
+            # if random.randint(0, 1):
+            j = random.randint(0, 6)
+            data[i][j] ^= 1
+        return data
     else:
         print("Error: Undifned Code Type !")
         return None
@@ -24,23 +34,24 @@ def randomRuin(data, codeType):
 # for hamming(8,4)
 class HammingCode():
     def __init__(self, rawData, rawLen=8):
-        global f
+        global f, rawString
         self.rawLen = rawLen
         # self.data = [[]]
-        self.data = [rawData[0+8*i:8+8*i]for i in range(0,int(self.rawLen/8))]
+        self.data = [rawData[0+8*i:8+8*i]for i in range(0, int(self.rawLen/8))]
         self.rawData = copy.deepcopy(self.data)
         # print(self.rawData)
-        self.data = [[-1,-1,self.data[i][0],-1,self.data[i][1],self.data[i][2],self.data[i][3],-1,self.data[i][4],self.data[i][5],self.data[i][6],self.data[i][7]] for i in range(0,int(self.rawLen/8))]
-        
+        self.data = [[-1, -1, self.data[i][0], -1, self.data[i][1], self.data[i][2], self.data[i][3], -1,
+                      self.data[i][4], self.data[i][5], self.data[i][6], self.data[i][7]] for i in range(0, int(self.rawLen/8))]
+
     def encode(self):
-        f.writelines(["Encoded Data:","\n"])
+        f.writelines(["Encoded Data:", "\n"])
         for i in range(0, int(self.rawLen/8)):
             hamming = []
             for j in range(0, 12):
                 if self.data[i][j] == 1:
                     hamming.append(j+1)
             hammingCode = 0
-            for t in range (0, len(hamming)):
+            for t in range(0, len(hamming)):
                 hammingCode ^= hamming[t]
             hammingCode = bin(hammingCode)[2:].zfill(4)
 
@@ -50,95 +61,264 @@ class HammingCode():
                         self.data[i][j] = ord(hammingCode[-t-1]) - ord('0')
                         break
             # write to file
-            f.writelines([str(self.data[i][j]) for j in range(len(self.data[i]))])
+            f.writelines([str(self.data[i][j])
+                          for j in range(len(self.data[i]))])
 
     def ruin(self):
-        self.data = randomRuin(self.data,"Hamming")
-        f.writelines(["\n","Defective Data:","\n"])
-        f.writelines([str(self.data[i][j]) for j in range(12) for i in range(int(self.rawLen/8))])
+        self.data = randomRuin(self.data, "Hamming")
+        f.writelines(["\n", "Defective Data:", "\n"])
+        f.writelines([str(self.data[i][j]) for j in range(12)
+                      for i in range(int(self.rawLen/8))])
 
     def decode(self):
-        f.writelines(["\n","Decoded Data:","\n"])
+        f.writelines(["\n", "Decoded Data:", "\n"])
         errorCount = 0
-        for i in range(0,int(self.rawLen/8)):
+        for i in range(0, int(self.rawLen/8)):
             hamming = []
             for j in range(0, 12):
                 if self.data[i][j] == 1:
                     hamming.append(j+1)
-            
+
             hammingCode = 0
-            for t in range (0, len(hamming)):
+            for t in range(0, len(hamming)):
                 hammingCode ^= hamming[t]
             if hammingCode == 0:
-                print ("No error")
+                print("No error")
             else:
-                print ("Error at %s" % (hammingCode-1))
+                print("Error at %s" % (hammingCode-1))
                 errorCount += 1
-                if hammingCode < 13 :
+                if hammingCode < 13:
                     self.data[i][hammingCode-1] ^= 1
                 else:
                     print("Can not correct!")
-            self.data[i] = [self.data[i][2], self.data[i][4], self.data[i][5], self.data[i][6], self.data[i][8], self.data[i][9], self.data[i][10], self.data[i][11]]
-            f.writelines([str(self.data[i][j]) for j in range(len(self.data[i]))])
+            self.data[i] = [self.data[i][2], self.data[i][4], self.data[i][5], self.data[i]
+                            [6], self.data[i][8], self.data[i][9], self.data[i][10], self.data[i][11]]
+            f.writelines([str(self.data[i][j])
+                          for j in range(len(self.data[i]))])
         # print(self.data)
-        f.writelines(["\n","Number of Error:",str(errorCount),", Corrected: ",str(self.data == self.rawData),"\n"])
+        f.writelines(["\n", "Number of Error:", str(errorCount),
+                      ", Corrected: ", str(self.getData() == rawString), "\n"])
 
     def getData(self):
-        return self.data
-
+        s = ""
+        for i in range(int(self.rawLen/8)):
+            for c in self.data[i]:
+                s += str(c)
+        return s
 
     # pass
 
 # for linear block(7,4)
+
+
 class LinearBlock():
     def __init__(self, rawData, rawLen=8):
-        global f
+        global f, rawString
         self.rawLen = rawLen
-        self.data = [rawData[0+4*i:4+4*i]for i in range(0,int(self.rawLen/4))]
+        self.data = [rawData[0+4*i:4+4*i]for i in range(0, int(self.rawLen/4))]
+        self.data = np.reshape(self.data, (int(self.rawLen/4), 4))
         self.rawdata = copy.deepcopy(self.data)
-        self.G =    [   [1, 0, 0, 0, 1, 1, 0],
-                        [0, 1, 0, 0, 0, 1, 1],
-                        [0, 0, 1, 0, 1, 1, 1],
-                        [0, 0, 0, 1, 1, 0, 1]
-                    ]
-        self.G = np.reshape(self.G,(4,7))
-        print(self.data)
+        self.G = [[1, 0, 0, 0, 1, 1, 0],
+                  [0, 1, 0, 0, 0, 1, 1],
+                  [0, 0, 1, 0, 1, 1, 1],
+                  [0, 0, 0, 1, 1, 0, 1]
+                  ]
+        self.G = np.reshape(self.G, (4, 7))
+        self.Ht = [[1, 0, 1, 1, 1, 0, 0],
+                   [1, 1, 1, 0, 0, 1, 0],
+                   [0, 1, 1, 1, 0, 0, 1]
+                   ]
+        self.Ht = np.reshape(self.Ht, (3, 7))
+        self.Ht = np.transpose(self.Ht)
+        self.errorMap = {6: 0,
+                         3: 1,
+                         7: 2,
+                         5: 3,
+                         4: 4,
+                         2: 5,
+                         1: 6
+                         }
 
     def encode(self):
-        f.writelines(["Encoded Data:","\n"])
+        f.writelines(["Encoded Data:", "\n"])
+        temp = np.zeros((int(self.rawLen/4), 7), 'i')
         for i in range(0, int(self.rawLen/4)):
-            print((np.dot(self.data[i], self.G))%2)
-            # TODO need to conduct in GF(2)
-        pass
+            temp[i] = (np.dot(self.data[i], self.G) % 2)
+        self.data = temp
+
+        for i in range(int(self.rawLen/4)):
+            f.writelines(str(c) for c in self.data[i])
 
     def ruin(self):
-        pass
+        self.data = randomRuin(self.data, "LinearBlock")
+        f.writelines(["\n", "Defective Data:", "\n"])
+        for i in range(int(self.rawLen/4)):
+            f.writelines(str(c) for c in self.data[i])
 
     def decode(self):
-        pass
+        f.writelines(["\n", "Decoded Data:", "\n"])
+        errorCount = 0
+        temp = np.zeros((int(self.rawLen/4), 4), 'i')
+        for i in range(int(self.rawLen/4)):
+            eHT = np.dot(self.data[i], self.Ht) % 2
+            if (np.sum(eHT) != 0):
+                errorCount += 1
+                eNum = self.errorMap[eHT[0] << 2 ^ eHT[1] << 1 ^ eHT[2]]
+                # print("Error at %s !" % eNum)
+                self.data[i][eNum] ^= 1
+            temp[i] = self.data[i][:4]
+        self.data = temp
+        for i in range(int(self.rawLen/4)):
+            f.writelines(str(c) for c in self.data[i])
+
+        f.writelines(["\n", "Number of Error:", str(errorCount),
+                      ", Corrected: ", str(self.getData() == rawString), "\n"])
 
     def getData(self):
-        pass
+        s = ""
+        for i in range(int(self.rawLen/4)):
+            for c in self.data[i]:
+                s += str(c)
+        return s
+
+# for cyclic(7,4)
+
+
+class CyclicCode():
+    def __init__(self, rawData, rawLen=8):
+        global f, rawString
+        self.rawLen = rawLen
+        self.data = [rawData[0+4*i:4+4*i]for i in range(0, int(self.rawLen/4))]
+        self.data = np.reshape(self.data, (int(self.rawLen/4), 4))
+        self.rawdata = copy.deepcopy(self.data)
+        # Note gx should be array below, but for convenience we assign another value
+        # self.gx = np.array([0, 0, 0, 1, 0, 1, 1])
+        self.gx = np.array([1, 0, 1, 1, 0, 0, 0])
+        self.mappingTable = {0: [0, 0, 0, 0, 0, 0, 0],
+                             1: [0, 0, 0, 1, 0, 1, 1],
+                             2: [0, 0, 1, 0, 1, 1, 0],
+                             3: [0, 0, 1, 1, 1, 0, 1],
+                             4: [0, 1, 0, 0, 1, 1, 1],
+                             5: [0, 1, 0, 1, 1, 0, 0],
+                             6: [0, 1, 1, 0, 0, 0, 1],
+                             7: [0, 1, 1, 1, 0, 1, 0],
+                             8: [1, 0, 0, 0, 1, 0, 1],
+                             9: [1, 0, 0, 1, 1, 1, 0],
+                             10: [1, 0, 1, 0, 0, 1, 1],
+                             11: [1, 0, 1, 1, 0, 0, 0],
+                             12: [1, 1, 0, 0, 0, 1, 0],
+                             13: [1, 1, 0, 1, 0, 0, 1],
+                             14: [1, 1, 1, 0, 1, 0, 0],
+                             15: [1, 1, 1, 1, 1, 1, 1]
+                             }
+
+        self.mappingTableBack = {0: [0, 0, 0, 0],
+                                11: [0, 0, 0, 1],
+                                22: [0, 0, 1, 0],
+                                29: [0, 0, 1, 1],
+                                39: [0, 1, 0, 0],
+                                44: [0, 1, 0, 1],
+                                49: [0, 1, 1, 0],
+                                58: [0, 1, 1, 1],
+                                69: [1, 0, 0, 0],
+                                78: [1, 0, 0, 1],
+                                83: [1, 0, 1, 0],
+                                88: [1, 0, 1, 1],
+                                98: [1, 1, 0, 0],
+                                105: [1, 1, 0, 1],
+                                116: [1, 1, 1, 0],
+                                127: [1, 1, 1, 1]
+                             }
+        
+        self.errorMap = {1: [0, 0, 0, 0, 0, 0, 1],
+                         2: [0, 0, 0, 0, 0, 1, 0],
+                         4: [0, 0, 0, 0, 1, 0, 0],
+                         3: [0, 0, 0, 1, 0, 0, 0],
+                         6: [0, 0, 1, 0, 0, 0, 0],
+                         7: [0, 1, 0, 0, 0, 0, 0],
+                         5: [1, 0, 0, 0, 0, 0, 0]
+                         }
+
+    def encode(self):
+        f.writelines(["Encoded Data:", "\n"])
+        temp = np.zeros((int(self.rawLen/4), 7), 'i')
+        for i in range(0, int(self.rawLen/4)):
+            temp[i] = self.mappingTable[self.data[i][0] << 3 ^ self.data[i][1] << 2 ^ self.data[i][2] << 1 ^ self.data[i][3]]
+        self.data = temp
+        f.writelines([self.getData(),"\n"])
+
+    def ruin(self):
+        self.data = randomRuin(self.data, "Cyclic")
+        f.writelines(["\n", "Defective Data:", "\n"])
+        f.writelines([self.getData(),"\n"])
+
+    def decode(self):
+        f.writelines(["\n", "Decoded Data:", "\n"])
+        errorCount = 0
+        temp = np.zeros((int(self.rawLen/4), 4), 'i')
+        for i in range(int(self.rawLen/4)):
+            remainder = (self.data[i][0] << 6) + (self.data[i][1] << 5) + (self.data[i][2] << 4) + (self.data[i][3] << 3) + (self.data[i][4] << 2) + (self.data[i][5] << 1) + (self.data[i][6])
+            temp_data = copy.deepcopy(self.data[i])
+            roll = 0
+            while remainder > 7:
+                while temp_data[roll] != 1:
+                    roll += 1
+                gx = np.roll(self.gx, roll)
+                temp_data = np.bitwise_xor(temp_data, gx)
+                remainder = (temp_data[0] << 6) + (temp_data[1] << 5) + (temp_data[2] << 4) + (temp_data[3] << 3) + (temp_data[4] << 2) + (temp_data[5] << 1) + (temp_data[6])
+                
+            if remainder != 0:
+                print (self.data[i])
+                print (self.errorMap[remainder])
+                self.data[i] = np.bitwise_xor(self.data[i], self.errorMap[remainder])
+                print ("Error Detect!")
+                errorCount += 1
+                print (self.data[i])
+            
+            temp[i] = np.array(self.mappingTableBack[(self.data[i][0] << 6) + (self.data[i][1] << 5) + (self.data[i][2] << 4) + (self.data[i][3] << 3) + (self.data[i][4] << 2) + (self.data[i][5] << 1) + (self.data[i][6])])
+        
+        self.data = temp
+        f.writelines([self.getData(),"\n"])
+        f.writelines(["\n", "Number of Error:", str(errorCount),
+                      ", Corrected: ", str(self.getData() == rawString), "\n"])
+
+
+    def getData(self):
+        s = ""
+        for i in range(int(self.rawLen/4)):
+            for c in self.data[i]:
+                s += str(c)
+        return s
 
 
 if __name__ == "__main__":
-    rawData = [random.randint(0,1) for i in range(64)]
-    f.writelines(["=========== Raw Data ===========","\n"])
-    f.writelines(["Raw Data:","\n"])
+    rawData = [random.randint(0, 1) for i in range(64)]
+    rawString = "".join([str(c) for c in rawData])
+    print(rawString)
+    f.writelines(["=========== Raw Data ===========", "\n"])
+    f.writelines(["Raw Data:", "\n"])
     f.writelines([str(rawData[i]) for i in range(len(rawData))])
     f.writelines("\n")
-    
-    print ("=========== Raw Data ===========")
-    print (rawData)
-    print("=============Hamming Code =============")
-    f.writelines(["=============Hamming Code =============","\n"])
-    myHamming = HammingCode(rawData,64)
+
+    print("=========== Raw Data ===========")
+    print(rawString)
+    print("============= Hamming Code =============")
+    f.writelines(["============= Hamming Code =============", "\n"])
+    myHamming = HammingCode(rawData, 64)
     myHamming.encode()
     myHamming.ruin()
     myHamming.decode()
-    print (myHamming.getData())
-    # print("=============Linear Block Code =============")
-    # f.writelines(["=============Linear Block Code =============","\n"])
-    # myLinearBlock = LinearBlock(rawData)
-    # myLinearBlock.encode()
-    pass
+    print(myHamming.getData())
+    print("============= Linear Block Code =============")
+    f.writelines(["============= Linear Block Code =============", "\n"])
+    myLinearBlock = LinearBlock(rawData,64)
+    myLinearBlock.encode()
+    myLinearBlock.ruin()
+    myLinearBlock.decode()
+    print(myLinearBlock.getData())
+    print("============= Cyclic Code =============")
+    f.writelines(["============= Cyclic Code =============", "\n"])
+    myCyclic = CyclicCode(rawData, 64)
+    myCyclic.encode()
+    myCyclic.ruin()
+    myCyclic.decode()
